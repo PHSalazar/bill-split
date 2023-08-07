3afadf02cbed333a5b28b198f6275cad7ee32112
ph.s.Salazar@gmail.com <ph.s.Salazar@gmail.com>
2023-08-07 19:35:24 -0300

    atualização

--- main.js
+++ main.js
@@ -1,1 +1,23 @@
-console.log('Hello World!');
+function addUserIn(user) {
+  var eleParent = document.getElementById('relatorio');
+  
+  // Criar elemento para usuário
+  elementP = document.createElement('p');
+  elementP.classList.add('card-post-item');
+  elementP.textContent = user.toLowerCase();
+  
+  // Criar notai para visualizar ou gerar relatório
+  elementButton = document.createElement('button');
+  elementButton.classList.add('card-post-button');
+  elementButton.textContent = 'Gerar';
+  
+  // Adicionar botão ao elemento criado
+  elementP.appendChild(elementButton);
+  
+  // Adicionar usuário ao documento
+  eleParent.appendChild(elementP);
+}
+
+addUserIn('Pedro');
+addUserIn('Milena');
+addUserIn('Guilherme');
